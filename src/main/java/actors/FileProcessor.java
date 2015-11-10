ec3d0b035eec64d01d3643a5c985b309028395e7
Artem Tsushko <artem.tsushko@gmail.com>
2015-11-10 21:14:54 +0200

    added actors and configuration

---
+++ src/main/java/actors/FileProcessor.java
@@ -0,0 +1,78 @@
+package actors;
+
+import akka.actor.ActorRef;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.routing.Broadcast;
+import akka.routing.FromConfig;
+import messages.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The master actor that, once received {@link messages.ProcessFile} message,
+ * reduces the input file containing strings "ID;amount"
+ * to output file containing string "ID;totalAmount"
+ * and then terminates the entire <code>ActorSystem</code>
+ *
+ * @author Artem Tsushko
+ * @version 1.0
+ */
+public class FileProcessor extends UntypedActor {
+
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private int linesSent = 0;
+    private Integer linesProcessed;
+    private ActorRef linesRouter;
+    private Map<String,BigDecimal> map;
+
+    @Override
+    public void preStart() throws Exception {
+        linesRouter = getContext().actorOf(
+                FromConfig.getInstance().props(Props.create(LineProcessor.class)),
+                "lineRouter");
+    }
+
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof ProcessFile) {
+            ProcessFile msg = (ProcessFile) message;
+            reader = new BufferedReader(new FileReader(msg.getInputFilePath()));
+            writer = new PrintWriter(msg.getOutputFilePath());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                linesRouter.tell(new ProcessLine(line), getSelf());
+                ++linesSent;
+            }
+            reader.close();
+            linesRouter.tell(new Broadcast(Finish.getInstance()), getSelf());
+        } else if (message instanceof LinesProcessed) {
+            LinesProcessed msg = (LinesProcessed) message;
+            if (linesProcessed == null) {
+                map = new HashMap<>(msg.getMap());
+                linesProcessed = msg.getLinesNumber();
+            } else {
+                msg.getMap().forEach((k,v)->map.merge(k,v,BigDecimal::add));
+                linesProcessed += msg.getLinesNumber();
+            }
+            if (linesProcessed == linesSent) {
+                linesRouter.tell(PoisonPill.getInstance(), getSelf());
+                map.forEach((k,v)->writer.println(k+";"+v));
+                writer.close();
+                getContext().system().terminate();
+            }
+        } else {
+            unhandled(message);
+        }
+    }
+
+}
+
---
+++ src/main/java/actors/LineProcessor.java
@@ -0,0 +1,51 @@
+package actors;
+
+import akka.actor.UntypedActor;
+import messages.Finish;
+import messages.LinesProcessed;
+import messages.ProcessLine;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * A worker actor, that maintains a map(ID;totalAmount),
+ * receives messages containing lines like "ID;amount",
+ * parses them and inserts into the map, incrementing
+ * the "totalAmount" value by "amount" in case of collision,
+ * and finally sends total number of processed lines and the map
+ * back to the master actor.
+ *
+ * @author Artem Tsushko
+ * @version 1.0
+ */
+public class LineProcessor extends UntypedActor{
+
+    private int linesProcessed = 0;
+    private Pattern separator = Pattern.compile(";");
+    private Map<String,BigDecimal> map;
+
+    @Override
+    public void preStart() throws Exception {
+        map = new HashMap<>();
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof ProcessLine) {
+            ProcessLine msg = (ProcessLine) message;
+            String[] parts = separator.split(msg.getLine());
+            String key = parts[0];
+            BigDecimal amount = new BigDecimal(parts[1]);
+            map.merge(key,amount,BigDecimal::add);
+            ++linesProcessed;
+        } else if (message instanceof Finish) {
+            LinesProcessed msg = new LinesProcessed(linesProcessed,map);
+            getSender().tell(msg, getSelf());
+        } else {
+            unhandled(message);
+        }
+    }
+}
