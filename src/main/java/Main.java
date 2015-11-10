751a3eb4ef53550a81d6d2851db0c0d224e5a068
Artem Tsushko <artem.tsushko@gmail.com>
2015-11-10 21:29:54 +0200

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import actors.FileProcessor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import messages.ProcessFile;
+
+/**
+ * Takes input and output file paths as command line arguments
+ * and reduces the input file of lines like "ID;amount"
+ * to the output file of lines like "ID;totalAmount"
+ *
+ * @author Artem Tsushko
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("fileReducer");
+        ActorRef fileProcessor = system.actorOf(
+                Props.create(FileProcessor.class),
+                "master");
+        fileProcessor.tell(new ProcessFile(args[0],args[1]),ActorRef.noSender());
+    }
+}
